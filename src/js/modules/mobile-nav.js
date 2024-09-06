81ae269a92455393c1e696b981674834166616ea
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2024-09-06 14:56:15 +0300

    fix: close burger after click

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,9 +1,16 @@
+let menuLinks = document.querySelectorAll(".nav-list-item");
+
 function mobileNav() {
 	var toggleButton = document.querySelector('.toggle-menu');
 	var navBar = document.querySelector('.nav-bar');
 	toggleButton.addEventListener('click', function () {
 		navBar.classList.toggle('toggle');
 	});
+	menuLinks.forEach((link) =>
+  	link.addEventListener("click", () => {
+		navBar.classList.toggle('toggle');
+  })
+);
 }
 
 export default mobileNav;
