2690f4a4cd25958963660fef436029b235088641
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2024-09-11 08:31:30 +0300

    rel: no photo version

--- src/mail.php
+++ src/mail.php
@@ -1,4 +1,5 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $email = $_POST["email"];
    $name = $_POST["name"];
    $phone = $_POST["phone"];
@@ -17,4 +18,12 @@
    $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
    $success = mail('show.elfproject@gmail.com', $subject, $message, $headers);
-   echo $success;
+   //echo $success;
+   echo "<script>window.history.back();
+   alert('Ваше сообщение отправлено. Мы свяжемся с вами в ближайшее время!');
+   </script>";
+exit();
+}
+?>
+
+<!-- show.elfproject@gmail.com -->
